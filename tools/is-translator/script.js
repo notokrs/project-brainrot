08d9b63ba8497861a37fae85c8a8754693620d49
notokrs <rusnoto030@gmail.com>
2024-06-17 21:37:11 +0700

    initial commit

---
+++ tools/is-translator/script.js
@@ -0,0 +1,58 @@
+const vocals = ["a", "i", "u", "e", "o"];
+const vReplace = ["p", "b"];
+
+const source = document.getElementById("text");
+const result = document.getElementById("result");
+
+const alert = document.getElementById("alert");
+const msg = document.getElementById("message");
+
+document.getElementById("btn-convert").addEventListener("click", function () {
+  const wordArr = source.value.toLowerCase().split(" ");
+
+  if (wordArr == "") {
+    msg.innerText = "Teks sumber kosong!";
+    alert.style.display = "block";
+
+    return;
+  }
+
+  const res = [];
+  for (let i = 0; i < wordArr.length; i++) {
+    let word = wordArr[i];
+    for (let j = 0; j < word.length; j++) {
+      if (
+        (j == 0 || j == 1 || j == 2) &&
+        j != word.length - 1 &&
+        word[j + 1] != "m" &&
+        vocals.includes(word[j]) &&
+        !vocals.includes(word[j + 1])
+      ) {
+        const wordSplit = word.split("");
+
+        wordSplit.splice(j + 1, 0, "m");
+        word = wordSplit.join("");
+      }
+
+      if (vReplace.includes(word[j])) {
+        const v = word.replace(word[j], "v");
+        word = v;
+      }
+    }
+
+    res.push(word);
+  }
+
+  result.value = res.join(" ");
+});
+
+document.getElementById("copy-result").addEventListener("click", () => {
+  if (result.value != "") {
+    navigator.clipboard.writeText(result.value);
+
+    msg.innerText = "Teks berhasil disalin!";
+    alert.style.display = "block";
+
+    return;
+  }
+});
